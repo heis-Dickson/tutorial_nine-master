79a7bf241db8eecd562d5adb507ec547135a018f
datboimayor <certifiedmayor6@gmail.com>
2025-02-11 11:26:52 -0700

    update

--- tny_script.js
+++ tny_script.js
@@ -10,8 +10,12 @@
    Date:   2025-02-06
 
 */
+//Invoke the function
+setInterval(
+   "runClock()",
+    );
 
-
+function runClock(){
 //Display the current date & time
 document.getElementById('dateNow').innerHTML = `m/d/y<br /> h:m:s`;
 
@@ -31,3 +35,20 @@ var timeStr = currentDay.toLocaleTimeString();
 //Display the current date & time
 document.getElementById('dateNow').innerHTML = 
 dateStr + '<br />' + timeStr;
+
+//Calculating the days until January 1st
+var newYear = new Date('January 1st, 2026');
+var nextYear = currentDay.getFullYear() + 1;
+newYear.setFullYear(nextYear);
+var daysLeft = (newYear - currentDay) / (1000 * 60 * 60 * 24);
+var hrsLeft = (daysLeft - Math.floor(daysLeft)) * 24;
+var minsLeft = (hrsLeft - Math.floor(hrsLeft)) * 60;
+var secsLeft = (minsLeft - Math.floor(minsLeft)) * 60;
+
+
+//Display the time left in the New Years Eve Countdown Clock
+document.getElementById('days').innerHTML = Math.floor(daysLeft);
+document.getElementById('hrs').innerHTML = Math.floor(hrsLeft);
+document.getElementById('mins').innerHTML = Math.floor(minsLeft);
+document.getElementById('secs').innerHTML = Math.floor(secsLeft);
+}
